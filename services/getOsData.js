948c90b31bf50454c4a71ccac571c517600a1251
Elismar Sivla <elismarsilva.5@outlook.com>
2020-04-15 10:25:40 -0300

    :bookmark: getting OS data

--- services/getOsData.js
+++ services/getOsData.js
@@ -1,1 +1,21 @@
-os
+const os = require('os');
+
+class SystemData extends os {
+    constructor() {
+        this.operationalSystem = this.getSystemData();
+        this.cpu = this.getCPUData();
+    }
+
+    getSystemData() {
+        return {
+            platform: super.platform(),
+            release: super.release()
+        }
+    }
+
+    getCPUData() {
+        const cpu = os.cpus();
+    }
+}
+
+module.exports = SystemData;
